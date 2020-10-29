954014651b36efdcc0d6d4663366aeefb1196801
Zarela Graves <zarelavp@yahoo.com>
2020-10-28 23:27:56 -0400

    Initial React setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import Note from './Note/Note'
+import './App.css'
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      notes: [
+        {id: 1, noteContent: 'This is note 1'},
+        {id: 2, noteContent: 'This is note 2'},
+      ]
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="notesWrapper">
+            <div className="notesHeader">
+              <div className="heading">Baby Graves</div>
+            </div>
+            <div className="notesBody">
+              {this.state.notes.map(note => {
+                return (
+                  <Note
+                    noteContent={note.noteContent}
+                    noteId={note.id}
+                    key={note.id}
+                  />
+                )}
+              )}
+            </div>
+            <div className="notesFooter">
+              Footer will be here
+            </div>
+          </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
