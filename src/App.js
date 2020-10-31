5037d9b8b6834b3296af5da4263fff0194e351db
Zarela Graves <zarelavp@yahoo.com>
2020-10-31 00:42:26 -0400

    Adds config and connection with Firebase

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import logo from './mini-unicorn.svg'
 import Note from './Note/Note'
 import NoteForm from './NoteForm/NoteForm'
+import Firebase from './Config/config'
+import firebase from 'firebase/app'
+import 'firebase/database'
 import './App.css'
 class App extends Component {
   constructor(props){
     super(props)
     this.addNote = this.addNote.bind(this)
 
+    this.app = Firebase
+
+    this.database = this.app.database().ref().child('notes')
+
     this.state = {
-      notes: [
-        {id: 1, noteContent: 'This is note 1'},
-        {id: 2, noteContent: 'This is note 2'},
-      ]
+      notes: []
     }
   }
 
-  addNote(note){
-    const previousNotes = this.state.notes;
-    previousNotes.push(
-      { id: previousNotes.length + 1, noteContent: note}
-    )
+  componentWillMount() {
+    const previousNotes = this.state.notes
 
-    this.setState({
-      notes: previousNotes
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
     })
+  }
+
+  addNote(note){
+    this.database.push().set({ noteContent: note })
   }
 
   render() {
