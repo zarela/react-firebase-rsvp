7728b3e3121ac2abffc78c56acae3fbb37418684
Zarela Graves <zarelavp@yahoo.com>
2020-11-11 00:27:25 -0500

    Adds favicon and changes styles

--- src/Registry/Registry.jsx
+++ src/Registry/Registry.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import amazonLogo from '../amazon-logo.png'
-import moonUnicorn from '../moon-unicorn.png'
+import donutUnicorn from '../donut-unicorn.jpg'
 import './Registry.css'
 
 const Registry = () => {
   return (
     <div className="center">
-      <p>
+      <p className="registry">
       Thank you so much for visiting our baby registry, and helping us get ready for our baby. More than anything, we are just so grateful for your love and support. Thank you!
       </p>
-      <img src={moonUnicorn} alt="moon-unicorn" height="180"/>
+      <img src={donutUnicorn} alt="donut-unicorn" height="180"/>
       <a href="https://www.amazon.com/baby-reg/35D30XMZOO914" rel="noreferrer" target="_blank" >
-        <img src={amazonLogo} alt="amazon-logo" width="300" className="amazon-logo"/>
+        <img src={amazonLogo} alt="amazon-logo" width="280" className="amazon-logo"/>
       </a>
       <a className="noteButton" href="https://www.amazon.com/baby-reg/35D30XMZOO914" rel="noreferrer" target="_blank" >Visit  our registry 	&#x2665;</a>
     </div>
--- src/ShowerDetails/ShowerDetails.jsx
+++ src/ShowerDetails/ShowerDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import moonUnicorn from '../moon-unicorn.png'
+import './ShowerDetails.css'
 
 const ShowerDetails = () => {
   return (
@@ -9,10 +10,22 @@ const ShowerDetails = () => {
       </p>
       <img src={moonUnicorn} alt="moon-unicorn" height="180"/>
       <p>
-        Day: Saturday November 21th<br />
-        Time: 5:00 PM - 6:30 PM<br />
-        Virtual via Zoom<br />
+        Day: Saturday November 21th<br/>
+        Time: 5:00 PM - 6:30 PM<br/>
+        Virtual via Zoom <br/>
+        Zoom Link: {''}
+         <a href="https://us02web.zoom.us/j/88078277688"  rel="noreferrer" target="_blank">Enter Here</a><br/>
+         Meeting ID: 880 7827 7688<br/>
       </p>
+      <div className="recommendations">
+        <p>
+          <h4>Recommendations:</h4>
+          Join in time of 5 mins earlier<br/>
+          Please keep your audio deactivated<br/>
+          Preferably use a computer to connect<br/>
+          Have a blank piece of paper and markers handy<br/>
+        </p>
+      </div>
     </div>
   )
 }
